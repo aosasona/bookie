f826ee3330ed50bcb271515e169afb354b784499
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-09 06:16:22 +0100

    Added custom exception and exception handler

--- app/src/main/java/com/trulyao/bookie/lib/AppException.kt
+++ app/src/main/java/com/trulyao/bookie/lib/AppException.kt
@@ -1,4 +1,22 @@
 package com.trulyao.bookie.lib
 
-class AppException {
+import android.content.Context
+
+class AppException : Exception {
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
+
+public fun handleException(context: Context, e: Exception) {
+    val message = when (e) {
+        is AppException -> e.message
+        else -> {
+            System.err.println(e.message)
+            System.err.println(e.stackTrace)
+            "Something went wrong, see logs for more details"
+        }
+    }
+
+    Alert.show(context, message!!)
 }
